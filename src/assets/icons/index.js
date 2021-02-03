9a58a5e57ad6199145e9c922cab6ecca9358752c
Junior331 <jarilson.rk@gmail.com>
2021-02-03 01:18:15 -0300

    Importing images into the project

---
+++ src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import Bag from "../icons/Bag.svg";
+import Group5 from "../icons/Group5.svg";
+import Logo from "../icons/Logo.svg";
+import Location from "../icons/Location.svg";
+import Lupa from "../icons/lupa.svg";
+import Heart from "../icons/heart.svg";
+import User from "../icons/user.svg";
+import Shape from "../icons/Shape.svg";
+import ShapeBlack from "../icons/Shape_black.svg";
+
+export { Bag, Group5, Logo, Location, Lupa, Heart, User, Shape, ShapeBlack };
---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import MulherImgCarousel from "../assets/MulherImgCarousel.svg";
+import PingenteImgCarousel from "../assets/PingenteImgCarousel.svg";
+import AliancaImgCarousel from "../assets/AliancaImgCarousel.svg";
+import aliancasImgCarousel from "../assets/aliancasImgCarousel.svg";
+import RelogioPulso from "../assets/relogio_no_pulso.svg";
+
+export {
+  RelogioPulso,
+  MulherImgCarousel,
+  PingenteImgCarousel,
+  AliancaImgCarousel,
+  aliancasImgCarousel,
+};
